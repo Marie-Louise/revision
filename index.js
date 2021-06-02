15b831822d8213db7fbc9e1cb6f2e152cff778c9
conniewambui <consolatawambuimacharia@gmail.com>
2021-06-02 18:18:30 +0300

    revision

---
+++ index.js
@@ -0,0 +1,108 @@
+function adder(a,b,c,d,e){
+    console.log(a+b+c+d+e)
+}
+adder(3,4,5,6,6)
+
+function specialGreeter(name){
+    console .log("hey" + name + "you are beautiful girl")
+   
+}
+specialGreeter( "Marie")
+
+console.log(34566654)
+
+var names=["Marie","Divine","Valentin","Olive"]
+console.log(names)  
+
+
+var sentence="Samantha is beautiful girl"
+console.log(sentence)
+
+var fruits=["apply","orange","banana",1,23,4,sentence]
+console.log(fruits)
+
+var Linda={
+    "name":"Linda is beatiful girl in kenya",
+    "age":2,
+    "professional":"PHD"
+   
+}
+console.log(Linda)
+var mgithinji={
+   "name":"mgithinji is my role model",
+   "age":2,
+   "professional":"PHD"
+}
+console.log(mgithinji)
+
+var num=5;
+if(num>0){
+    console.log("the number is a positive.")
+}
+var num=-5;
+if(num<0){
+    console.log("the number is a negative.")
+}
+
+// // if (num >0){
+// //  alert("the number is positive")
+// // }else if (num<0){
+// //   alert("the number is negative.")  
+// // }else{
+// //     // alert("teh number is a zero")
+// // }
+
+// function printerArray(mango){
+//     for (var i=0;i<mango.length;i++){
+//         console.log(mango[i])
+
+//     }
+
+// }
+// console.log("sweet","cat","racco")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // if (num >0){
+// //  alert("the number is positive")
+// // }else if (num<0){
+// //   alert("the number is negative.")  
+// // }else{
+// //     // alert("teh number is a zero")
+// // }
+
+// function printerArray(mango){
+//     for (var i=0;i<mango.length;i++){
+//         console.log(mango[i])
+
+//     }
+
+// }
+// console.log("sweet","cat","racco")
+
+
